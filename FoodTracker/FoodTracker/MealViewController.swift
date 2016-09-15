93ea47ab372ecaec671ce1625117b0b74fce829e
Jonathon Fishman <JonF@Jons-MacBook-Pro.local>
2016-09-15 16:12:17 -0500

    Created Functioning TableView
    Created TableView with custom cells to display the data we collect in the Meal View Controller
    Added sample data
    Fixed a but with Meal.swift which wasn’t hooked up correctly to the project file

--- FoodTracker/FoodTracker/MealViewController.swift
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Jonathon Fishman on 9/14/16.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
-    
-    @IBOutlet weak var mealNameLabel: UILabel!
     
     @IBOutlet weak var photoImageView: UIImageView!
     
@@ -45,7 +43,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     
     func textFieldDidEndEditing(textField: UITextField) {
         
-        mealNameLabel.text = textField.text
     }
     
     // MARK: UIImagePickerControllerDelegate
--- FoodTracker/FoodTrackerTests/Meal.swift
+++ FoodTracker/FoodTrackerTests/Meal.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2016 GoYoJo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            
+            // Because the initializer now might return nil, you need to indicate this in the initializer signature.
+            return nil
+        }
+    
+    }
+    
+    
+    
+}
