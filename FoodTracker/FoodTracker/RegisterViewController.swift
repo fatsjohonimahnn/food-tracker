7ac428dc4611cf660910bff125acfcc6eb0e848a
Jonathon Fishman <JonF@Jons-MacBook-Pro.local>
2016-09-27 17:51:45 -0500

    Update RegisterVC

--- FoodTracker/FoodTracker/RegisterViewController.swift
+++ FoodTracker/FoodTracker/RegisterViewController.swift
@@ -14,13 +14,17 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordConfirmTextField: UITextField!
-    
     @IBOutlet weak var registerBtn: UIButton!
+    
+    let backendless = Backendless.sharedInstance()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // adds ability to check if textFields have certain input
+        emailTextField.addTarget(self, action: #selector(LoginViewController.textFieldChanged(textField:)), for: UIControlEvents.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(LoginViewController.textFieldChanged(textField:)), for: UIControlEvents.editingChanged)
+        passwordConfirmTextField.addTarget(self, action: #selector(LoginViewController.textFieldChanged(textField:)), for: UIControlEvents.editingChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,9 +32,63 @@ class RegisterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+//    // helper function to check if textFields have input, used in viewDidLoad
+//    func textFieldChanged(textField: UITextField) {
+//        
+//        if emailTextField.text == "" || passwordTextField.text == "" || passwordConfirmTextField.text == "" {
+//            registerBtn.isEnabled = false
+//        } else {
+//            registerBtn.isEnabled = true
+//        }
+//    }
+    
     @IBAction func register(_ sender: UIButton) {
+        
+        if passwordTextField.text != passwordConfirmTextField.text {
+            Utility.showAlert(viewController: self, title: "Registration Error", message: "Password confirmation failed. Please enter your password try again.")
+            return
+        }
+        
+        if !Utility.isValidEmail(emailAddress: emailTextField.text!) {
+            Utility.showAlert(viewController: self, title: "Registration Error", message: "Please enter a valid email address")
+            return
+        }
+        
+        spinner.startAnimating()
+        
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        BackendlessManager.sharedInstance.registerUser(email: email, password: password , completion: {
+            
+            BackendlessManager.sharedInstance.loginUser(email: email, password: password , completion: {
+                
+                    self.spinner.stopAnimating()
+                
+                    self.performSegue(withIdentifier: "registerToTV", sender: sender)
+                },
+                                                        
+                error: { message in
+                    
+                    self.spinner.stopAnimating()
+                    
+                    Utility.showAlert(viewController: self, title: "Login Error", message: message)
+                })
+            
+            },
+            
+            error: { message in
+        
+                self.spinner.stopAnimating()
+        
+                Utility.showAlert(viewController: self, title: "Register Error", message: message)
+        })
     }
     
     @IBAction func cancel(_ sender: UIButton) {
+        
+        spinner.stopAnimating()
+        
+        dismiss(animated: true, completion: nil)
     }
 }
